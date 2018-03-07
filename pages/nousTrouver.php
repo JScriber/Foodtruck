161184f5976ad6ba4da1a091fd10f79d1dedab9a
Jscriber <anthonygst26@gmail.com>
2018-03-07 09:53:52 +0100

    Adresse et nom adresse

--- pages/nousTrouver.php
+++ pages/nousTrouver.php
@@ -23,12 +23,48 @@
         </ul>
         <div class="allMap">
            <div class="slider">
-        		<div data-latitude="48.046807" data-longitude="-1.740896" class="map"></div>
-        		<div data-latitude="48.085075" data-longitude="-1.691362" class="map"></div>
-        		<div data-latitude="48.121466" data-longitude="-1.704052" class="map"></div>
-        		<div data-latitude="48.115891" data-longitude="-1.637143" class="map"></div>
-        		<div data-latitude="48.125957" data-longitude="-1.623550" class="map"></div>
-        		<div data-latitude="48.125924" data-longitude="-1.650284" class="map"></div>
+                <ul>
+                    <li>
+						<h2>Campus de Kerlann</h2>
+						<p>5 rue des jolies pierres poncées</p>
+                    </li>
+                    <li data-latitude="48.046807" data-longitude="-1.740896" class="map"></li>
+                </ul>
+                <ul>
+                    <li>
+						<h2>Campus de xorg</h2>
+						<p>8 rue des monstres</p>
+					</li>
+        			<li data-latitude="48.085075" data-longitude="-1.691362" class="map"></li>
+        		</ul>
+        		<ul>
+					<li>
+						<h2>Rennes 8</h2>
+						<p>50 rue des rennais</p>
+					</li>
+        			<li data-latitude="48.121466" data-longitude="-1.704052" class="map"></li>
+        		</ul>
+        		<ul>
+        			<li>
+						<h2>Campus de New York City</h2>
+						<p>78 rue Steven Awkward Taison</p>
+					</li>
+        			<li data-latitude="48.115891" data-longitude="-1.637143" class="map"></li>
+        		</ul>
+        		<ul>
+        			<li>
+						<h2>Truc much</h2>
+						<p>Rue inconnu</p>
+					</li>
+        			<li data-latitude="48.125957" data-longitude="-1.623550" class="map"></li>
+        		</ul>
+        		<ul>
+        			<li>
+						<h2>Boulevard des rois</h2>
+						<p>12 rue des princes</p>
+					</li>
+        			<li data-latitude="48.125924" data-longitude="-1.650284" class="map"></li>
+        		</ul>
             </div>
         </div>
         <div id="showEvents">
