bc02c549ed6d6bfcdf5f2a5c3e8bd8d8ab0ca459
Jscriber <anthonygst26@gmail.com>
2018-03-07 09:13:12 +0100

    Plus d'insertions

--- bdd/insertion_bdd_calm.sql
+++ bdd/insertion_bdd_calm.sql
@@ -3,15 +3,17 @@
 #------------------------------------------------------------
 
 INSERT INTO prix VALUES (NULL, "2.50");
+INSERT INTO prix VALUES (NULL, "10");
 
 
 INSERT INTO plat VALUES (NULL, "Sandwich maison", "sandwich.jpg", "Le sandwich maison est un super bon sandwich que nous préparons avec amour chaque jour.", FALSE, FALSE, TRUE, 1);
 
+INSERT INTO plat VALUES (NULL, "Bolognaise", "bolognaise.jpg", "Les bolognaises biologiques que vous allez adorer !", FALSE, FALSE, FALSE, 2);
 
-INSERT INTO ingredient VALUES (NULL, "pain"), (NULL, "beurre"),(NULL, "jambon"),(NULL, "salade");
+INSERT INTO ingredient VALUES (NULL, "pain"), (NULL, "beurre"),(NULL, "jambon"),(NULL, "salade"),(NULL, "pâtes");
 
 
-INSERT INTO possede VALUES (1, 1), (1,2), (1,3), (1,4);
+INSERT INTO possede VALUES (1, 1), (1,2), (1,3), (1,4), (2,5), (2,3);
 
 
 INSERT INTO evenement VALUES (NULL, "Art Rock", "https://www.artrock.org/"), (NULL, "Bobital", "https://www.bobital-festival.fr/");
